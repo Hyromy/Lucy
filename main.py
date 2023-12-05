e2caddc0c91f51d8608cf8fd985a4a55f980784b
Hyromy <formulajoel9@gmail.com>
2023-12-04 20:55:01 -0600

    unirse y salirse de canales de voz

--- main.py
+++ main.py
@@ -28,4 +28,19 @@ async def on_member_remove(member):
 async def hola(ctx):
     await ctx.send("Hola!")
     
+@client.command(pass_context = True)
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("Tienes que estar primero en un canal de voz para acompañarte")
+
+@client.command(pass_context = True)
+async def leave(ctx):
+    if ctx.author.voice:
+        await ctx.guild.voice_client.disconnect()
+    else:
+        await ctx.send("No he llegado y ya me estan corriendo :c")
+    
 client.run(TOKEN)
